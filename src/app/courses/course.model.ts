b712b986ac36e50a85def64e4908975b39a238f9
Artem <artemgolovachov1256789@gmail.com>
2025-03-29 18:55:12 +0200

    make section in main pages

--- src/app/courses/course.model.ts
+++ src/app/courses/course.model.ts
@@ -1,14 +1,17 @@
+import { Section } from "../shared/section.model";
 
 
 export class Course{
   public name: string;
   public description: string;
   public imagePath: string;
+  public sections: Section[];
   
-  constructor(name: string, desc: string, imagePath: string){
+  constructor(name: string, desc: string, imagePath: string, sections: Section[]){
     this.description = desc;
     this.name = name;
     this.imagePath = imagePath;
+    this.sections = sections;
   }
 
 }
