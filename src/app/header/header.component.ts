56e3a3eefd0ce4eaec480148c01fa898142cb0f7
Artem <artemgolovachov1256789@gmail.com>
2025-05-17 22:30:26 +0300

    update visivle part of "detail-component"

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  // styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit, OnDestroy{
   isAuthenticated = false;
